1cc3751087d3b00ce3e87b8aed05301c5d5c8b00
Andreville1 <andrevillegas_09@hotmail.com>
2024-05-05 22:56:10 -0600

    Added description section

--- churris-banca-web/src/Components/Posts/Posts.jsx
+++ churris-banca-web/src/Components/Posts/Posts.jsx
@@ -1,30 +1,17 @@
 import React from "react";
-import { CgProfile } from "react-icons/cg";
-import { GoDotFill } from "react-icons/go";
-import { BsThreeDots } from "react-icons/bs";
-import "./Posts.css";
+import Head from "./Head";
 
 const Posts = () => {
   return (
     <div id="posts">
-      <div id="head">
-        <div id="data">
-          <div className="profile-picture">
-            <CgProfile />
-          </div>
-          <div id="name-hour">
-            <div id="name">
-              <text>User 1</text>
-              <GoDotFill id="icon-small" />
-              <text id="smaller-text">Unfollow</text>
-            </div>
-            <text id="hour">1 h</text>
-          </div>
-        </div>
+      <Head />
 
-        <div id="three-dots">
-          <BsThreeDots />
-        </div>
+      <div id="description">
+        <text>Insert text here</text>
+        <br />
+        <text className="blue-text">#tagOne #tagTwo #tagThree</text>
+        <br />
+        <text className="blue-text">https://enter-url.com</text>
       </div>
     </div>
   );
