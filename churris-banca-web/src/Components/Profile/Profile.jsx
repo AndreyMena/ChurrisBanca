ef9aeec51ad3e6c5bbb762c3da2a4a4b3ca4467d
Cristian Ortega <cristian.ortegahurtado@ucr.ac.cr>
2024-05-08 03:11:39 -0600

    Profile component updated using Material UI.

--- churris-banca-web/src/Components/Profile/Profile.jsx
+++ churris-banca-web/src/Components/Profile/Profile.jsx
@@ -1,8 +1,19 @@
 import React from "react";
 import { CgProfile } from "react-icons/cg";
-import { IoCallOutline } from "react-icons/io5";
 import { CiMail, CiCircleInfo } from "react-icons/ci";
+import IconButton from '@mui/material/IconButton';
+import TextField from '@mui/material/TextField';
+import EditIcon from '@mui/icons-material/Edit';
+import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
+import PasswordOutlinedIcon from '@mui/icons-material/PasswordOutlined';
+import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
+import PhoneAndroidOutlinedIcon from '@mui/icons-material/PhoneAndroidOutlined';
+
 import "./Profile.css";
+
+const onCLickHandle = () => {
+
+}
 
 const Profile = () => {
   return (
@@ -10,16 +21,45 @@ const Profile = () => {
       <div id="big-profile-picture">
         <CgProfile id="img" />
       </div>
-      <text>User 5</text>
+      <TextField
+          id="standard-read-only-input"
+          defaultValue="Fullname"
+          InputProps={{
+            readOnly: true,
+          }}
+          variant="standard"
+        />
       <div id="information">
         <div className="specific-information">
-          <CiMail />
-          <text className="text">user5@gmail.com</text>
+          <PersonOutlineOutlinedIcon className="user-information-icons" fontSize="large" />
+          <TextField disabled id="username-text-field" label="Username"  variant="standard" />
+          <IconButton>
+            <EditIcon color="action" />
+          </IconButton>
         </div>
 
         <div className="specific-information">
-          <IoCallOutline />
-          <text className="text">12-34-56-78</text>
+          <PasswordOutlinedIcon className="user-information-icons" fontSize="large" />
+          <TextField disabled id="password-text-field" label="Password" variant="standard" />
+          <IconButton>
+            <EditIcon color="action"/>
+          </IconButton>
+        </div>
+
+        <div className="specific-information">
+          <EmailOutlinedIcon className="user-information-icons" fontSize="large" />
+          <TextField disabled id="email-text-field" label="Email" variant="standard" />
+          <IconButton>
+            <EditIcon color="action"/>
+          </IconButton>
+        </div>
+
+        <div className="specific-information">
+          <PhoneAndroidOutlinedIcon className="user-information-icons" fontSize="large" />
+          <TextField disabled id="phone-number-text-field" label="Cell phone number" variant="standard" />
+          <IconButton>
+            <EditIcon color="action"/>
+          </IconButton>
         </div>
 
         <div className="specific-information">
