0707a9b0aada19a93fd81f4f7162c9672b8dff8f
Cristian Ortega <cristian.ortegahurtado@ucr.ac.cr>
2024-05-11 18:22:25 -0600

    Home page functionality added to web page name.

--- churris-banca-web/src/Components/Header/Header.jsx
+++ churris-banca-web/src/Components/Header/Header.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useNavigate } from 'react-router-dom';
 import AccountBalanceOutlinedIcon from '@mui/icons-material/AccountBalanceOutlined';
 import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
+import Button from '@mui/material/Button';
 import Diversity3OutlinedIcon from '@mui/icons-material/Diversity3Outlined';
 import IconButton from '@mui/material/IconButton';
 import SearchEngine from "../Tags/SearchEngine";
@@ -11,38 +12,40 @@ import "./Header.css";
 const Header = () => {
   const navigate = useNavigate();
 
-  const handleAccountBalanceClick = () => {
+  const handleHomeClick = () => {
     navigate('/');
   }
 
+  const handleTransactionsClick = () => {
+    navigate('/');
+  }
+  
   const handleSocialClick = () => {
     navigate('/socialPage');
   }
 
-  const handleAccountClick = () => {
+  const handleProfileClick = () => {
     navigate('/userProfile');
-  }
+  } 
 
   return (
     <div id="header-container">
       <div id="logo-search-container">
-        <text>
-          <b>Churris Banca</b>
-        </text>
+        <Button size="large" variant="text" onClick={handleHomeClick} style={{ fontWeight: 'bold', fontSize: "20px"}}>Churris Banca</Button>
         <SearchEngine text="Buscar" id="search-engine" />
       </div>
 
       <div id="menu-container">
-        <IconButton className="icon-button-header" color="primary" onClick={handleAccountBalanceClick}>
-          <AccountBalanceOutlinedIcon></AccountBalanceOutlinedIcon>
+        <IconButton className="icon-button-header" color="primary" onClick={handleTransactionsClick}>
+          <AccountBalanceOutlinedIcon fontSize="large"></AccountBalanceOutlinedIcon>
         </IconButton>
 
         <IconButton className="icon-button-header" color="primary" onClick={handleSocialClick}>
-          <Diversity3OutlinedIcon></Diversity3OutlinedIcon>
+          <Diversity3OutlinedIcon fontSize="large"></Diversity3OutlinedIcon>
         </IconButton>
 
-        <IconButton className="icon-button-header" color="primary" onClick={handleAccountClick}>
-          <AccountCircleOutlinedIcon></AccountCircleOutlinedIcon>
+        <IconButton className="icon-button-header" color="primary" onClick={handleProfileClick}>
+          <AccountCircleOutlinedIcon fontSize="large"></AccountCircleOutlinedIcon>
         </IconButton>
       </div>
     </div>
