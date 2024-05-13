71620a4a647c7e85776312513ae0fefec50a7733
Andreville1 <andrevillegas_09@ucr.ac.cr>
2024-05-13 17:38:04 -0600

    Added prop-types to the project

--- churris-banca-web/src/Components/UserInformationField/UserInformationField.jsx
+++ churris-banca-web/src/Components/UserInformationField/UserInformationField.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import PropTypes from 'prop-types'
 import TextField from '@mui/material/TextField';
 import IconButton from '@mui/material/IconButton';
 import EditIcon from '@mui/icons-material/Edit';
@@ -74,5 +75,12 @@ const UserInformationField = ({id, label, defaultValue, type = "text"}) => {
     </div>
   );
 };
+
+UserInformationField.propTypes = {
+  id: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  defaultValue: PropTypes.string.isRequired,
+  type: PropTypes.string
+}
 
 export default UserInformationField;
