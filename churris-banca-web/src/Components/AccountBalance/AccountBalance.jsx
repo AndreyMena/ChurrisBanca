3428de21567330ef8c924e9adabc77e6f910f2d6
Cristian Ortega <cristian.ortegahurtado@ucr.ac.cr>
2024-05-13 01:06:36 -0600

    Función para solicitar el balance de la cuenta al servidor con mensajes en formato JSON listas. Función hecha con hooks useState y useEffect,  y con axios para los mensajes entre el server.

--- churris-banca-web/src/Components/AccountBalance/AccountBalance.jsx
+++ churris-banca-web/src/Components/AccountBalance/AccountBalance.jsx
@@ -1,13 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import  Typography from '@mui/material/Typography';
 
 import "./AccountBalance.css";
 
 const AccountBalance = () => {
   // Hay que recibir el mensaje desde el servidor que contiene los datos del balance/saldo de la cuenta obtenidos desde la base de datos y asignar esos datos a las siguientes variables para luego renderizar el componente AccountBalance ya con esos datos.
-  const accountId = "XXXXXXXXXX";
-  const typeCurrency = "Churruminos";
-  const accountBalance = "Ch 15,500.00";
+  const [accountId, setAccountId] = useState("XXXXXXXXXX");
+  const [typeCurrency, setTypeCurrency] = useState("Churruminos");
+  const [accountBalance, setAaccountBalance] = useState("Ch 15,500.00");
+
+  const userCredentials = {
+    type: "getUserAccountBalance",
+    user: "userName",
+    password: "password"
+  };
+
+  useEffect(() => {
+    console.log("use effect");
+    // Realiza la solicitud al servidor, hay que definir la ruta al servidor
+    axios.post("/ruta-al-servidor", userCredentials)
+
+      .then(response => {
+        // Accede al valor de los atributos dentro del mensaje JSON para luego asignarlos a variables.
+        const id = response.data.accountId;
+        const currency = response.data.accountId;
+        const balance = response.data.accountId;
+        // Con las variales se actuliza el valor de los useState
+        setAccountId(id);
+        setTypeCurrency(currency);
+        setAaccountBalance(balance);
+      })
+      .catch(error => {
+        // Maneja los errores si la solicitud falla
+        console.error("Error al realizar la conexión o recibir el texto del servidor:", error);
+      });
+  }, []);
 
   return (
     <div id="account-container">
