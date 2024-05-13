c6afb6ed50863bf396c7f862cddb8aaa97729a4b
AndreyMena <andreyest.12@gmail.com>
2024-05-13 00:04:47 -0600

    Se agrega proyecto backend node, con lo necesario para el login, jwt, cookies, logout y demas. FALTA conectar con mariadb por ahora se prueba con el user.json

---
+++ churris-banca-backend/middleware/verifyJWT.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+
+//Middleware para TODO lo que entra
+const verifyJWT = (req, res, next) => {
+    const authHeader = req.headers.authorization || req.headers.Authorization;
+
+    // Verificacion de seguridad del header para auth
+    //if (!authHeader) return res.sendStatus(401);
+    if (!authHeader?.startsWith('Bearer ')) return res.sendStatus(401);
+    const token = authHeader.split(' ')[1];
+    console.log(token)
+    jwt.verify(
+        token,
+        process.env.ACCESS_TOKEN_SECRET,
+        (err, decoded) => {
+            if (err) return res.sendStatus(403); //Token invalido
+            req.user = decoded.username;
+            next();
+        }
+    );
+}
+
+module.exports = verifyJWT
