9c15b872de2d3d9ac967b9e6f7354fa6ea4e0c85
Andreville1 <andrevillegas_09@hotmail.com>
2024-05-18 21:57:42 -0600

    Banking refactoring

--- churris-banca-web/src/pages/BankingPage/BankingPage.jsx
+++ churris-banca-web/src/pages/BankingPage/BankingPage.jsx
@@ -5,10 +5,7 @@ import PaginationBar from "../../Components/PaginationBar/PaginationBar";
 import Transaction from "../../Components/Transaction/Transaction";
 import ClientServerProvider from "../../Components/Providers/ClientServerProvider";
 
-
-export const handleTransactions = (messageFromServer) => {
-
-}
+export const handleTransactions = (messageFromServer) => {};
 
 // Elimminar despues
 const data = [
@@ -18,7 +15,7 @@ const data = [
     transactionUser: "Paulina Rodriguez Jimenez",
     transactionDate: "11-04-2024",
     transactionTime: "13:08",
-    transactionAmount: 5500
+    transactionAmount: 5500,
   },
   {
     transactionID: "56781234",
@@ -26,7 +23,7 @@ const data = [
     transactionUser: "Roberto Chavez Madriz",
     transactionDate: "12-02-2022",
     transactionTime: "12:12",
-    transactionAmount: 2000
+    transactionAmount: 2000,
   },
   {
     transactionID: "43215678",
@@ -34,7 +31,7 @@ const data = [
     transactionUser: "Ramina Chavez Gonzalez",
     transactionDate: "12-03-2021",
     transactionTime: "10:25",
-    transactionAmount: 2500
+    transactionAmount: 2500,
   },
   {
     transactionID: "13572468",
@@ -42,7 +39,7 @@ const data = [
     transactionUser: "Mario Bermudez Fuentes",
     transactionDate: "15-02-2023",
     transactionTime: "09:09",
-    transactionAmount: 1200
+    transactionAmount: 1200,
   },
   {
     transactionID: "97531246",
@@ -50,7 +47,7 @@ const data = [
     transactionUser: "Maria Rodriguez Hernandez",
     transactionDate: "12-10-2022",
     transactionTime: "15:20",
-    transactionAmount: 1400
+    transactionAmount: 1400,
   },
   {
     transactionID: "11223344",
@@ -58,7 +55,7 @@ const data = [
     transactionUser: "Josefo Villareal Sanchez",
     transactionDate: "07-01-2020",
     transactionTime: "18:00",
-    transactionAmount: 10000
+    transactionAmount: 10000,
   },
   {
     transactionID: "12356748",
@@ -66,9 +63,9 @@ const data = [
     transactionUser: "Fernando Villareal Sibaja",
     transactionDate: "01-04-2021",
     transactionTime: "20:20",
-    transactionAmount: 10200
+    transactionAmount: 10200,
   },
-]
+];
 
 const BankingPage = () => {
   return (
@@ -76,13 +73,21 @@ const BankingPage = () => {
       <div id="banking-page-container">
         <Header />
         <div className="container">
-          <AccountBalance></AccountBalance>
+          <AccountBalance />
+
           {data.map((transaction) => (
-            <Transaction key={transaction.transactionID} transactionID={transaction.transactionID} transactionType={transaction.transactionType} transactionUser={transaction.transactionUser}
-              transactionDate={transaction.transactionDate} transactionTime={transaction.transactionTime} transactionAmount={transaction.transactionAmount} />
+            <Transaction
+              key={transaction.transactionID}
+              transactionID={transaction.transactionID}
+              transactionType={transaction.transactionType}
+              transactionUser={transaction.transactionUser}
+              transactionDate={transaction.transactionDate}
+              transactionTime={transaction.transactionTime}
+              transactionAmount={transaction.transactionAmount}
+            />
           ))}
 
-          <PaginationBar></PaginationBar>
+          <PaginationBar />
         </div>
       </div>
     </ClientServerProvider>
