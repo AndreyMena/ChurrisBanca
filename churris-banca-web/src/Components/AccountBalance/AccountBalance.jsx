6f734e825673c4285d66377038e840f97a97c5b4
Andreville1 <andrevillegas_09@hotmail.com>
2024-05-18 21:58:09 -0600

    Added button for send money

--- churris-banca-web/src/Components/AccountBalance/AccountBalance.jsx
+++ churris-banca-web/src/Components/AccountBalance/AccountBalance.jsx
@@ -1,12 +1,11 @@
 import React, { useState, useEffect } from "react";
+import { Button } from "@mui/material";
+import Typography from "@mui/material/Typography";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 import axios from "axios";
-import  Typography from '@mui/material/Typography';
-
 import "./AccountBalance.css";
 
-export const handleAccountBalance = (messageFromServer) => {
-
-}
+export const handleAccountBalance = (messageFromServer) => {};
 
 const AccountBalance = () => {
   // Hay que recibir el mensaje desde el servidor que contiene los datos del balance/saldo de la cuenta obtenidos desde la base de datos y asignar esos datos a las siguientes variables para luego renderizar el componente AccountBalance ya con esos datos.
@@ -17,15 +16,16 @@ const AccountBalance = () => {
   const userCredentials = {
     type: "getUserAccountBalance",
     user: "userName",
-    password: "password"
+    password: "password",
   };
 
   useEffect(() => {
     console.log("use effect");
     // Realiza la solicitud al servidor, hay que definir la ruta al servidor
-    axios.post("/ruta-al-servidor", userCredentials)
+    axios
+      .post("/ruta-al-servidor", userCredentials)
 
-      .then(response => {
+      .then((response) => {
         // Accede al valor de los atributos dentro del mensaje JSON para luego asignarlos a variables.
         const id = response.data.accountId;
         const currency = response.data.accountId;
@@ -35,18 +35,32 @@ const AccountBalance = () => {
         setTypeCurrency(currency);
         setAaccountBalance(balance);
       })
-      .catch(error => {
+      .catch((error) => {
         // Maneja los errores si la solicitud falla
-        console.error("Error al realizar la conexión o recibir el texto del servidor:", error);
+        console.error(
+          "Error al realizar la conexión o recibir el texto del servidor:",
+          error
+        );
       });
   }, []);
 
   return (
     <div id="account-container">
-      <div id="account-id-container">
+      <div className="info">
         <Typography>Account ID: {accountId}</Typography>
       </div>
-      <div id="account-balance-container">
+      <div className="info">
+        <Button id="button-send-money">
+          <div id="info-button">
+            Send money
+            <div id="icon">
+              Ch
+              <ArrowForwardIcon />
+            </div>
+          </div>
+        </Button>
+      </div>
+      <div className="info">
         <Typography>Balance of {typeCurrency}</Typography>
         <Typography>{accountBalance}</Typography>
       </div>
