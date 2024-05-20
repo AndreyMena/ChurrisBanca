4b1c09ef6d13859f3869dddec9e52f95cfa7a96e
AndreyMena <andreyest.12@gmail.com>
2024-05-20 00:06:49 -0600

    Se conecta con mariadb nodejs y se hacen consultas del login

--- churris-banca-backend/controllers/authController.js
+++ churris-banca-backend/controllers/authController.js
@@ -8,42 +8,35 @@ const bcrypt = require('bcrypt');  //Encriptador y desemcriptador
 const jwt = require('jsonwebtoken');  //Tokens para la sesion
 const fsPromises = require('fs').promises;
 const path = require('path');
+const pool = require('../config/dbConnection');
 
 const handleLogin = async (req, res) => {
     const { user, pwd } = req.body;
     if (!user || !pwd) return res.status(400).json({ 'message': 'Username and password are required.' });
-
-    const foundUser = usersDB.users.find(person => person.username === user);  //TODO Cambiar por operacion a mariaDB
+    
+    const sqlQuery = 'SELECT Email, Contrasena, RefreshToken FROM USUARIO WHERE Email=?';
+    const foundUser = await pool.query(sqlQuery, user);
     if (!foundUser) return res.sendStatus(401); //Unauthorized 
     
     // Evalua password 
-    const match = await bcrypt.compare(pwd, foundUser.password);
+    const match = await bcrypt.compare(pwd, foundUser[0].Contrasena);
     if (match) {
-
-        // TODO
-        //const roles = Object.values(foundUser.roles).filter(Boolean);
-
         // Create JWTs
         const accessToken = jwt.sign(
-            { "username": foundUser.username },
+            { "username": foundUser[0].Email },
             process.env.ACCESS_TOKEN_SECRET,
             { expiresIn: '15m' }  // Cambiar a 15min, osea '15m'
         );
         const refreshToken = jwt.sign(
-            { "username": foundUser.username },
+            { "username": foundUser[0].Email },
             process.env.REFRESH_TOKEN_SECRET,
             { expiresIn: '50m' }
         );
         
         // Guarda refreshToken del usuario
         // TODO: por ahora escribe en JSON, luego cambiar a la Base
-        const otherUsers = usersDB.users.filter(person => person.username !== foundUser.username);
-        const currentUser = { ...foundUser, refreshToken };
-        usersDB.setUsers([...otherUsers, currentUser]);
-        await fsPromises.writeFile(
-            path.join(__dirname, '..', 'model', 'users.json'),
-            JSON.stringify(usersDB.users)
-        );
+        const sqlQuery = 'UPDATE USUARIO SET RefreshToken = ? WHERE Email = ?';
+        await pool.query(sqlQuery, [refreshToken, foundUser[0].Email]);
 
         // Se crea el cookie con el refreshtoken, httpOnly para que no lo saquen con
         // JS, el calculo del max age es para 1 dia.
