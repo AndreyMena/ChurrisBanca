b882f6e51cb060d36e241b5370f7303e1f9517d2
Andreville1 <andrevillegas_09@hotmail.com>
2024-05-19 22:04:17 -0600

    Changed component Profile to work with useAccountStore

--- churris-banca-backend/controllers/accountController.js
+++ churris-banca-backend/controllers/accountController.js
@@ -2,11 +2,33 @@ const { response } = require("express");
 
 // Borrar después
 const accounts = [
-  { id: 1, userName: "jose.castro", accountStatus: 1550000, currency: "Ch" },
-  { id: 2, userName: "maria.hernandez", accountStatus: 150000, currency: "€" },
+  {
+    id: 1,
+    userName: "jose.castro.rojas",
+    name: "Jose",
+    lastName: "Castro Rojas",
+    email: "jose.castro.rojas@gmail.com",
+    phoneNumber: "12345678",
+    accountStatus: 1550000,
+    currency: "Ch",
+  },
+  {
+    id: 2,
+    userName: "maria.hernandez.sanchez",
+    name: "Maria",
+    lastName: "Hernandez Sanchez",
+    email: "maria.hernandez.sanchez@gmail.com",
+    phoneNumber: "34567890",
+    accountStatus: 150000,
+    currency: "€",
+  },
   {
     id: 2,
     userName: "roberto.chavez.madriz",
+    name: "Roberto",
+    lastName: "Chavez Madriz",
+    email: "roberto.chavez.madriz@gmail.com",
+    phoneNumber: "56789043",
     accountStatus: 150000,
     currency: "Ch",
   },
--- churris-banca-web/src/Components/UserInformationField/UserInformationField.jsx
+++ churris-banca-web/src/Components/UserInformationField/UserInformationField.jsx
@@ -1,35 +1,41 @@
 import React, { useState } from "react";
 import Button from "@mui/material/Button";
-import IconButton from '@mui/material/IconButton';
-import EditIcon from '@mui/icons-material/Edit';
-import PropTypes from 'prop-types'
-import TextField from '@mui/material/TextField';
+import IconButton from "@mui/material/IconButton";
+import EditIcon from "@mui/icons-material/Edit";
+import PropTypes from "prop-types";
+import TextField from "@mui/material/TextField";
 
 import "./UserInformationField.css";
 
-const UserInformationField = ({id, label, defaultValue, onChange, type = "text"}) => {
+const UserInformationField = ({
+  id,
+  label,
+  defaultValue,
+  onChange,
+  type = "text",
+}) => {
   const [isTextFieldDisabled, setIsTextFieldDisabled] = useState(true);
   const [isEditIconVisible, setIsEditIconVisible] = useState(true);
   const [isSaveButtonVisible, setIsSaveButtonVisible] = useState(false);
-  const [text, setText] = useState('');
+  const [text, setText] = useState("");
 
   const handleEnableTextField = () => {
     setIsTextFieldDisabled(false);
     setIsEditIconVisible(false);
     setIsSaveButtonVisible(true);
-  }
+  };
 
   const handleChange = (event) => {
     setText(event.target.value);
-  }
+  };
 
   const handleSaveClick = (event) => {
     console.log(text);
-    
+
     setIsTextFieldDisabled(true);
     setIsEditIconVisible(true);
     setIsSaveButtonVisible(false);
-  }
+  };
 
   return (
     <div className="user-information-editing-container">
@@ -39,14 +45,27 @@ const UserInformationField = ({id, label, defaultValue, onChange, type = "text"}
         id={id}
         type={type}
         label={label}
-        defaultValue={defaultValue}
+        value={defaultValue}
         variant="standard"
         onChange={(e) => onChange(e.target.value)}
       />
-      {isEditIconVisible && <IconButton className="user-information-editing-components" onClick={handleEnableTextField}>
-        <EditIcon color="action"/>
-      </IconButton>}
-      {isSaveButtonVisible && <Button className="user-information-editing-components" variant="outlined" onClick={handleSaveClick}>Save</Button>}
+      {isEditIconVisible && (
+        <IconButton
+          className="user-information-editing-components"
+          onClick={handleEnableTextField}
+        >
+          <EditIcon color="action" />
+        </IconButton>
+      )}
+      {isSaveButtonVisible && (
+        <Button
+          className="user-information-editing-components"
+          variant="outlined"
+          onClick={handleSaveClick}
+        >
+          Save
+        </Button>
+      )}
     </div>
   );
 };
@@ -55,7 +74,7 @@ UserInformationField.propTypes = {
   id: PropTypes.string.isRequired,
   label: PropTypes.string.isRequired,
   defaultValue: PropTypes.string.isRequired,
-  type: PropTypes.string
-}
+  type: PropTypes.string,
+};
 
 export default UserInformationField;
