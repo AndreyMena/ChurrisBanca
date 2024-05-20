f5c349580a5b56d46dbf58efb6ae32dcd1aeecbd
Cristian Ortega <cristian.ortegahurtado@ucr.ac.cr>
2024-05-20 16:20:27 -0600

    Estilos y componentes actualizados para crear un post.

--- churris-banca-web/src/Components/CreatePost/CreatePost.jsx
+++ churris-banca-web/src/Components/CreatePost/CreatePost.jsx
@@ -1,15 +1,34 @@
 import React from "react";
-import { CgProfile } from "react-icons/cg";
-import SearchEngine from "../Tags/SearchEngine";
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
+import IconButton from "@mui/material/IconButton";
+import InputAdornment from '@mui/material/InputAdornment';
+import InsertPhotoOutlinedIcon from '@mui/icons-material/InsertPhotoOutlined';
+import TextField from "@mui/material/TextField";
+
 import "./CreatePost.css";
 
 const CreatePost = () => {
   return (
-    <div id="create-post">
-      <div className="profile-picture">
-        <CgProfile />
+    <div id="create-post-container">
+      <div className="create-post-item-container">
+        <AccountCircleOutlinedIcon fontSize="large"></AccountCircleOutlinedIcon>
       </div>
-      <SearchEngine text="¿Qué estás pensando, User?" id="search-engine" />
+      <div className="create-post-item-container">
+        <TextField 
+          className="custom-text-field" 
+          placeholder="What's on your mind?" 
+          variant="filled"
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton>
+                  <InsertPhotoOutlinedIcon ></InsertPhotoOutlinedIcon>
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
+        ></TextField>
+      </div>
     </div>
   );
 };
