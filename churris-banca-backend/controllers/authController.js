4f00f60d77cffe151d4046264169dbaa9598af2f
Andreville1 <andrevillegas_09@hotmail.com>
2024-05-25 18:21:39 -0600

    Modified the context in order to add the userName in it

--- churris-banca-backend/controllers/authController.js
+++ churris-banca-backend/controllers/authController.js
@@ -1,44 +1,54 @@
-const bcrypt = require('bcrypt');  //Encriptador y desemcriptador
-const jwt = require('jsonwebtoken');  //Tokens para la sesion
-const pool = require('../config/dbConnection');
+const bcrypt = require("bcrypt"); //Encriptador y desemcriptador
+const jwt = require("jsonwebtoken"); //Tokens para la sesion
+const pool = require("../config/dbConnection");
 
 const handleLogin = async (req, res) => {
-    const { user, pwd } = req.body;
-    if (!user || !pwd) return res.status(400).json({ 'message': 'Username and password are required.' });
-    
-    const sqlQuery = 'SELECT Email, Contrasena, RefreshToken FROM USUARIO WHERE Email=?';
-    const foundUser = await pool.query(sqlQuery, user);
-    if (!foundUser) return res.sendStatus(401); //Unauthorized 
-    
-    // Evalua password 
-    const match = await bcrypt.compare(pwd, foundUser[0].Contrasena);
-    if (match) {
-        // Create JWTs
-        const accessToken = jwt.sign(
-            { "username": foundUser[0].Email },
-            process.env.ACCESS_TOKEN_SECRET,
-            { expiresIn: '15m' }  // Cambiar a 15min, osea '15m'
-        );
-        const refreshToken = jwt.sign(
-            { "username": foundUser[0].Email },
-            process.env.REFRESH_TOKEN_SECRET,
-            { expiresIn: '50m' }
-        );
-        
-        // Guarda refreshToken del usuario
-        const sqlQuery = 'UPDATE USUARIO SET RefreshToken = ? WHERE Email = ?';
-        await pool.query(sqlQuery, [refreshToken, foundUser[0].Email]);
+  const { email, pwd } = req.body;
+  if (!email || !pwd)
+    return res
+      .status(400)
+      .json({ message: "Username and password are required." });
 
-        // Se crea el cookie con el refreshtoken, httpOnly para que no lo saquen con
-        // JS, el calculo del max age es para 1 dia.
-        // Quitar secure si probamos con postman o thunderclient, pero hay q volverlo a poner
-        res.cookie('jwt', refreshToken, { httpOnly: true, secure: true, sameSite: 'None', maxAge: 24 * 60 * 60 * 1000 });
+  const sqlQuery =
+    "SELECT Nickname, Email, Contrasena, RefreshToken FROM USUARIO WHERE Email=?";
+  const foundUser = await pool.query(sqlQuery, email);
+  if (!foundUser) return res.sendStatus(401); //Unauthorized
 
-        // Send authorization roles and access token to user
-        res.json({ accessToken });
-    } else {
-        res.sendStatus(401);
-    }
-}
+  // Evalua password
+  const match = await bcrypt.compare(pwd, foundUser[0].Contrasena);
+  if (match) {
+    // Create JWTs
+    const accessToken = jwt.sign(
+      { username: foundUser[0].Email },
+      process.env.ACCESS_TOKEN_SECRET,
+      { expiresIn: "15m" } // Cambiar a 15min, osea '15m'
+    );
+    const refreshToken = jwt.sign(
+      { username: foundUser[0].Email },
+      process.env.REFRESH_TOKEN_SECRET,
+      { expiresIn: "50m" }
+    );
+
+    // Guarda refreshToken del usuario
+    const sqlQuery = "UPDATE USUARIO SET RefreshToken = ? WHERE Email = ?";
+    await pool.query(sqlQuery, [refreshToken, foundUser[0].Email]);
+
+    // Se crea el cookie con el refreshtoken, httpOnly para que no lo saquen con
+    // JS, el calculo del max age es para 1 dia.
+    // Quitar secure si probamos con postman o thunderclient, pero hay q volverlo a poner
+    res.cookie("jwt", refreshToken, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "None",
+      maxAge: 24 * 60 * 60 * 1000,
+    });
+
+    const user = foundUser[0].Nickname;
+    // Send authorization roles and access token to user
+    res.json({ accessToken, user });
+  } else {
+    res.sendStatus(401);
+  }
+};
 
 module.exports = { handleLogin };
