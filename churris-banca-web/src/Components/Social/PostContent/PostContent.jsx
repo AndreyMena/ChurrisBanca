a58e85cacde3206368b2b68e20a79d0e863d8592
Andreville1 <andrevillegas_09@hotmail.com>
2024-05-26 11:51:47 -0600

    Modified styles of the PostContent component

--- churris-banca-web/src/Components/Social/PostContent/PostContent.jsx
+++ churris-banca-web/src/Components/Social/PostContent/PostContent.jsx
@@ -1,16 +1,20 @@
 import React from "react";
-import Box from "@mui/material/Box"
+import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 
-import "./PostContent.css"
+import "./PostContent.css";
 
 const PostContent = ({ postContent, postImage }) => {
-    // TODO: Borrar este código y cambiarlo por el comentado cuando la conexión React-Node-MariaDB funcione.
+  // TODO: Borrar este código y cambiarlo por el comentado cuando la conexión React-Node-MariaDB funcione.
   return (
     <div id="post-content-container">
-      <Typography>Content of post.</Typography>
-      <Box id="image-box" component="img" src="https://ps.w.org/user-avatar-reloaded/assets/icon-128x128.png?rev=2540745"></Box>
-    
+      <Typography id="text">Content of post.</Typography>
+      <Box
+        id="image-box"
+        component="img"
+        src="https://ps.w.org/user-avatar-reloaded/assets/icon-128x128.png?rev=2540745"
+      ></Box>
+
       {/* <Typography>{postContent}</Typography>
       <Box id="image-box" component="img" src={postImage}></Box> */}
     </div>
