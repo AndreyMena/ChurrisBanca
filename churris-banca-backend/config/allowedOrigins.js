4f2eb6b998743fd6fe4a11ce3dc0e2ce73d47de0
AndreyMena <andreyest.12@gmail.com>
2024-05-30 14:19:52 -0600

    Origen 3000 permitido

--- churris-banca-backend/config/allowedOrigins.js
+++ churris-banca-backend/config/allowedOrigins.js
@@ -2,6 +2,7 @@ const allowedOrigins = [
   'http://localhost:443',  //App react
   'http://localhost:8443',  //Mismo node, Quitar?
   'http://localhost:80',
+  'http://localhost:3000',
   'http://localhost',  
   'http://172.24.131.193:443',
   'http://172.24.131.193:8443', 
