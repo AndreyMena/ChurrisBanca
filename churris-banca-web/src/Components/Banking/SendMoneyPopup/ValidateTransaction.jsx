01b321dc6f2c9278c54e9d8a1821e58c7a35cf91
Andreville1 <andrevillegas_09@hotmail.com>
2024-06-01 11:52:30 -0600

    Added style to key up button

--- churris-banca-web/src/Components/Banking/SendMoneyPopup/ValidateTransaction.jsx
+++ churris-banca-web/src/Components/Banking/SendMoneyPopup/ValidateTransaction.jsx
@@ -1,14 +1,16 @@
-import React, { useState, useEffect } from "react";
-import { Button, Typography } from "@mui/material";
+import React, { useState, useEffect, useRef } from "react";
+import { Button, IconButton, Typography } from "@mui/material";
+import { UploadOutlined } from "@mui/icons-material";
 import useAuth from "../../../hooks/useAuth";
 import { useBankStore } from "../../../hooks/useBankStore";
 import "./SendMoneyPopup.css";
 
-const ValidateTransaction = ({selectedContact, amount, handlePrevStage}) => {
+const ValidateTransaction = ({ selectedContact, amount, handlePrevStage }) => {
   const { startCreatingTransaction, resMsg } = useBankStore();
   const { auth } = useAuth();
   const [keyFile, setKeyFile] = useState(null);
   const [showResMsg, setShowResMsg] = useState(false);
+  const fileInputRef = useRef();
 
   const handleKeyChange = (event) => {
     setKeyFile(event.target.files[0]);
@@ -22,13 +24,13 @@ const ValidateTransaction = ({selectedContact, amount, handlePrevStage}) => {
       formData.append("key", keyFile);
       formData.append("nicknameCuentaDestino", selectedContact);
       formData.append("amount", amount);
-      formData.append("userName", auth.user)
+      formData.append("userName", auth.user);
 
       startCreatingTransaction(formData);
       setShowResMsg(true);
     }
   };
-  
+
   useEffect(() => {
     if (showResMsg) {
       setTimeout(() => {
@@ -41,14 +43,33 @@ const ValidateTransaction = ({selectedContact, amount, handlePrevStage}) => {
     <form id="form-key" onSubmit={handleSubmit}>
       <label>
         Key:
-        <input id="input-key" type="file" name="key" accept=".key" onChange={handleKeyChange} />
+        <input
+          id="input-key"
+          type="file"
+          name="key"
+          accept=".key"
+          ref={fileInputRef}
+          onChange={handleKeyChange}
+          style={{ display: "none" }}
+        />
+        <IconButton onClick={() => fileInputRef.current.click()}>
+          <UploadOutlined />
+        </IconButton>
       </label>
-      {showResMsg && resMsg && <Typography id={resMsg === "Transaction succesful" ? "success-msg" : "error-msg"}>{resMsg}</Typography>}
+      {showResMsg && resMsg && (
+        <Typography
+          id={resMsg === "Transaction succesful" ? "success-msg" : "error-msg"}
+        >
+          {resMsg}
+        </Typography>
+      )}
       <div id="buttons-container">
         <Button className="btn" onClick={handlePrevStage}>
-              Prev
+          Prev
         </Button>
-        <Button className="btn" type="submit">Submit Transaction</Button>
+        <Button className="btn" type="submit">
+          Submit Transaction
+        </Button>
       </div>
     </form>
   );
