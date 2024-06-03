51908e38f42839e70decb92f886925ea03c8be6d
Andreville1 <andrevillegas_09@ucr.ac.cr>
2024-06-03 16:43:47 -0600

    Added validations for putNewLike method

--- churris-banca-backend/controllers/socialController.js
+++ churris-banca-backend/controllers/socialController.js
@@ -120,10 +120,24 @@ const putNewPost = async (req, res = response) => {
 };
 
 const putNewLike = async (req, res = response) => {
-  const { userName, postId } = req.body;
+  try {
+    const { userName, postId } = req.body;
+    if (!userName || !postId) {
+      return res.status(400).json({ message: "Missing required fields" });
+    }
 
-  const sqlQuery = `INSERT INTO LIKES (IdMensaje, Nickname) VALUES (?, ?);`;
-  await pool.query(sqlQuery, [postId, userName]);
+    const sqlQuery = `INSERT INTO LIKES (IdMensaje, Nickname) VALUES (?, ?);`;
+    await pool.query(sqlQuery, [postId, userName]);
+
+    res.status(200).json({ message: "Like updated successfully" });
+  } catch (error) {
+    if (error.errno === 1452) { // 1452 signaling a violation of a foreign key constraint
+      return res.status(400).json({ message: "Message not found or no changes made" });
+    }
+
+    res.status(500).json({ message: "Internal server error" });
+    throw new Error(error);
+  }
 };
 
 const putNewDislike = async (req, res = response) => {
